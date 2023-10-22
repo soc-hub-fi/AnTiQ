d9b3ed13b1dcac908caa1d657d5ac09fd89bb443
Antti Nurmi <antti.nurmi@tuni.fi>
2023-10-22 14:11:53 +0300

    Verilator tb compiling

--- sim/scripts/verilate.sh
+++ sim/scripts/verilate.sh
@@ -1,7 +1,11 @@
 verilator --cc  ../src/array_pq/pq_pkg.svh \
 		../src/array_pq/pq_cell_fsm.sv \
 		../src/array_pq/pq_cell.sv \
-		../src/array_pq/pq.sv
+		../src/array_pq/pq.sv \
+		--top-module pq \
+		-Wall --trace --exe ../verilator/tb_pq.cc
+
+make -C obj_dir -f Vpq.mk Vpq
 
 RESULT=$?
 if [ $RESULT -eq 0 ]; then
--- sim/verilator/tb_pq.cc
+++ sim/verilator/tb_pq.cc
@@ -7,13 +7,14 @@ Example testbench template
 #include <verilated.h>
 #include <verilated_vcd_c.h>
 //#include "Valu.h"
+#include "Vpq.h"
 //#include "Valu___024unit.h"
 
 #define MAX_SIM_TIME 20
 vluint64_t sim_time = 0;
 
 int main(int argc, char** argv, char** env) {
-    Valu *dut = new Valu;
+    Vpq *dut = new Vpq;
 
     Verilated::traceEverOn(true);
     VerilatedVcdC *m_trace = new VerilatedVcdC;
@@ -21,7 +22,7 @@ int main(int argc, char** argv, char** env) {
     m_trace->open("waveform.vcd");
 
     while (sim_time < MAX_SIM_TIME) {
-        dut->clk ^= 1;
+        dut->clk_i ^= 1;
         dut->eval();
         m_trace->dump(sim_time);
         sim_time++;
