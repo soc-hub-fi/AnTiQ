fa4e0c9375124dd56407d9d5ce28736d52d4dbb1
Antti Nurmi <antti.nurmi@tuni.fi>
2025-07-08 21:22:21 +0300

    Modify interface

--- src/apb_antiq.sv
+++ src/apb_antiq.sv
@@ -1,6 +1,8 @@
 module apb_antiq #()(
-  input  logic clk_i,
-  input  logic rst_ni
+  input  logic        clk_i,
+  input  logic        rst_ni,
+  input  logic [63:0] mtime_i,
+  APB.Slave           apb_sbr
 );
 
 pq #()i_pq_core (
